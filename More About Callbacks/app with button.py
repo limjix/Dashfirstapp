d309c8ec233344c37740b54beefa38f7dd573425
limjix <limjix@gmail.com>
2019-11-13 14:09:36 +0800

    first commit

---
+++ More About Callbacks/app with button.py
@@ -0,0 +1,40 @@
+'''
+dash.dependencies.State allows you to pass along extra values without firing the callbacks
+
+Note that we're triggering the callback by listening to the n_clicks property of the html.
+Button component. n_clicks is a property that gets incremented every time the component has been clicked on.
+It is available in every component in the dash_html_components library.
+
+'''
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Input(id='input-1-state', type='text', value='Montréal'),
+    dcc.Input(id='input-2-state', type='text', value='Canada'),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    html.Div(id='output-state')
+])
+
+
+@app.callback(Output('output-state', 'children'),
+              [Input('submit-button', 'n_clicks')],
+              [State('input-1-state', 'value'),
+               State('input-2-state', 'value')])
+def update_output(n_clicks, input1, input2):
+    return u'''
+        The Button has been pressed {} times,
+        Input 1 is "{}",
+        and Input 2 is "{}"
+    '''.format(n_clicks, input1, input2)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
